9186838bad68996051b4a9d9250b75185434f9d6
Aldo Ziflaj <aldo.ziflaj@leadfeeder.com>
2023-01-29 16:27:55 +0100

    ran some analysis, avg of 27 len

--- main.go
+++ main.go
@@ -6,17 +6,13 @@ import (
 	"fmt"
 )
 
-func main() {
-	bitsy := 0b0010
-	fmt.Println(bitsy)
-	fmt.Println(bitsy << 1)
-	fmt.Println(bitsy << 2)
-
-	deck := cardistry.NewDeck()
-	deck.Shuffle()
-
-	fmt.Println(deck)
-
+// Compress the deck
+// @return sign: false means the series starts with Black,
+//
+//	true means it starts with Red
+//
+// @return compressArray: the array of the compressed deck
+func compress(deck *cardistry.Deck) (bool, []uint8) {
 	// false means the series starts with Black,
 	// true means it starts with Red
 	sign := false
@@ -41,14 +37,49 @@ func main() {
 				compressArray[prevIndex] += 1
 			}
 		}
+
 	}
 
-	if sign {
-		fmt.Println("starts with Red")
-	} else {
-		fmt.Println("starts with Black")
+	return sign, compressArray
+}
+
+func main() {
+	// experiments with bit shifting
+	// bitsy := 0b0010
+	// fmt.Println(bitsy)
+	// fmt.Println(bitsy << 1)
+	// fmt.Println(bitsy << 2)
+
+	sum := 0
+	ca := 0
+
+	// Deck magistry
+	deck := cardistry.NewDeck()
+
+	const trials = 100000
+	// do the magic many times
+	for i := 0; i < trials; i++ {
+		deck.Shuffle()
+		_, compressArray := compress(deck)
+
+		caSum := len(compressArray)
+		sum += caSum
+
+		if i%100 == 0 {
+			fmt.Println("CumAvg", ca)
+			fmt.Println("Count", i)
+			fmt.Println("Length of last run", len(compressArray))
+
+			ca = ca + (caSum-ca)/(i+1)
+			fmt.Println("CumAvg", ca)
+		} else {
+			ca = ca + (caSum-ca)/(i+1)
+		}
+
 	}
 
-	fmt.Println(len(compressArray))
-	fmt.Println(compressArray)
+	fmt.Println("CumAvg", ca)
+	fmt.Println("Sum", sum)
+	fmt.Println("Avg", sum/trials)
+
 }
